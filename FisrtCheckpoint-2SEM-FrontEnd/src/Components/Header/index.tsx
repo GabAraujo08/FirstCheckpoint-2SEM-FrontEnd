26d79bfdb7d0f67d2c2f2844bacab42d76e8f737
Gabriel Araujo da Silva Brito <rm558802@fiap.com.br>
2024-08-14 22:57:39 -0300

    continuando checkpoint

--- FisrtCheckpoint-2SEM-FrontEnd/src/Components/Header/index.tsx
+++ FisrtCheckpoint-2SEM-FrontEnd/src/Components/Header/index.tsx
@@ -1,19 +1,20 @@
 import imgLogo from '../../assets/img/imgLogo.svg'
+import toggleMenu from '../../assets/img/toggleMenu.svg'
 import './style.css'
 
 const Header = () => {
   return (
     <header>
         <nav>
-        <img className="imgLogo" src={imgLogo} alt="" />
-        <div className="menu">
-            <i className="toggleBar">
-            
-            </i>
-            <p>Menu</p>
-        </div>
-        <input className="inputBusca" type="search"/>
-        <button className="btnLogin">Login</button>
+          <img className="imgLogo" src={imgLogo} alt="" />
+          <div className="menu">
+              <i className="toggleBar">
+              <img className="imgLogo" src={toggleMenu} alt="" />
+              </i>
+              <p>Menu</p>
+          </div>
+          <input className="inputBusca" type="search"/>
+          <button className="btnLogin">Login</button>
         </nav>
         
     </header>
