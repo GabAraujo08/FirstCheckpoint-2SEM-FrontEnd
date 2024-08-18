b77f9eacf1b4f7da8eafdf9ec03036c3a6a9c926
Gabriel Araujo da Silva Brito <rm558802@fiap.com.br>
2024-08-18 12:35:37 -0300

    continuando banner

--- FisrtCheckpoint-2SEM-FrontEnd/src/Components/Banner/index.tsx
+++ FisrtCheckpoint-2SEM-FrontEnd/src/Components/Banner/index.tsx
@@ -1,11 +1,34 @@
-
+import './style.css'
+import bannerImg from '../../assets/img/backgroundGow.png'
+import iconStarFull from '../../assets/img/IconStarFull.svg'
+import iconStar from '../../assets/img/IconStar.svg'
 
 const Banner = () => {
   return (
-    <section>
-        <h1>Ola</h1>
-    </section>
-    
+    <main>
+      <section>
+        <div className='headerBanner'>
+          <h1>GOD OF WAR: RAGNAROK</h1>
+          <div className="boxAvaliacoes">
+            <div className='avaliacaoRatings'>
+              <h1>AVALIAÇÃO DA RATINGS</h1>
+              <div>
+                <i><img src={iconStarFull} alt="" /></i>
+                <p>9,6/10</p>
+              </div>
+            </div>
+            <div className='suaAvaliacao'>
+              <h1>SUA AVALIAÇÃO</h1>
+              <div>
+                <i><img src={iconStar} alt="" /></i>
+                <p>Avaliar</p>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </section>
+    </main>
   );
 }
 
