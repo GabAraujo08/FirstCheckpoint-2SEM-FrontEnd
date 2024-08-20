9c6dfb05820cdfa6c1a56bda74ddfe98a4ab15ec
Gabriel Araujo da Silva Brito <rm558802@fiap.com.br>
2024-08-20 18:19:58 -0300

    Iniciando avaliações de usuários

--- FisrtCheckpoint-2SEM-FrontEnd/src/Components/Avaliacoes/index.tsx
+++ FisrtCheckpoint-2SEM-FrontEnd/src/Components/Avaliacoes/index.tsx
@@ -1,10 +1,22 @@
 import './style.css'
-
+import iconArrow from '../../assets/img/iconArrow.svg'
+import iconMais from '../../assets/img/iconMais.svg'
 
 const Avaliacoes = () => {
-  return (
-    <h1>Olá</h1>
-  );
+    return (
+        <section className='avaliacoes'>
+            <div className="headerAvaliacoes">
+                <div className="imagemEtitulo">
+                    <img src={iconArrow} alt="" />
+                    <h1>Avaliações de usuários</h1>
+                </div>
+                <div className='avaliar'>
+                    <img src={iconMais} alt="" />
+                    <h1>Avaliar</h1>
+                </div>
+            </div>
+        </section>
+    );
 }
 
 export default Avaliacoes
